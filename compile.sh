527cebc2c046ba2d29b8336aee7d77407391813f
marcos <marcos@192.168.1.135>
2017-12-12 22:23:16 +0100

    Actualizados los scripts para poder compiltar y ejecutar.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 chmod 777 src/es/ubu/proyecto/model/*
 chmod 777 src/es/ubu/proyecto/management/*
 chmod 777 src/es/ubu/proyecto/storage/*
-chmod 777 src/es/ubu/proyecto/textui/*
+chmod 777 src/es/ubu/proyecto/UI/*
+chmod 777 src/es/ubu/proyecto/UI/GUI/*
+chmod 777 src/es/ubu/proyecto/UI/textUI/*
 
 mkdir -p bin
-javac -classpath .:src/:bin/:. -d bin/ src/es/ubu/proyecto/model/Producto.java src/es/ubu/proyecto/model/Linea.java src/es/ubu/proyecto/model/ListaCompra.java src/es/ubu/proyecto/management/*.java src/es/ubu/proyecto/storage/*.java src/es/ubu/proyecto/textui/*.java
+javac -classpath .:src/:bin/:. -d bin/ src/es/ubu/proyecto/model/*.java src/es/ubu/proyecto/management/*.java src/es/ubu/proyecto/storage/*.java src/es/ubu/proyecto/UI/*.java src/es/ubu/proyecto/UI/GUI/*.java src/es/ubu/proyecto/UI/textUI/*.java
--- execute.sh
+++ execute.sh
@@ -1,6 +1,8 @@
-chmod 777 bin/es/ubu/proyecto/model/*
-chmod 777 bin/es/ubu/proyecto/management/* 
-chmod 777 bin/es/ubu/proyecto/storage/*
-chmod 777 bin/es/ubu/proyecto/textui/*
+chmod 777 src/es/ubu/proyecto/model/*
+chmod 777 src/es/ubu/proyecto/management/*
+chmod 777 src/es/ubu/proyecto/storage/*
+chmod 777 src/es/ubu/proyecto/UI/*
+chmod 777 src/es/ubu/proyecto/UI/GUI/*
+chmod 777 src/es/ubu/proyecto/UI/textUI/*
 
-java -cp bin/ es.ubu.proyecto.textui.TextMain
+java -cp bin/ es.ubu.proyecto.UI.LaunchList
